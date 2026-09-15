efd531e2652cbfa093835b7e279183a30a95f4a6
Mahsa Keikha <mahsa.keikha@gmail.com>
2026-09-15 15:57:24 -0700

    Protect researcher LinkedIn profile links

--- tests/test_about_researcher_website.py
+++ tests/test_about_researcher_website.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
 CONNECTED_CARE_URL = 'https://www.connectioncare.org/'
+LINKEDIN_URL = 'https://www.linkedin.com/in/mahsa-keikha-ph-d-p-eng-a9470b43/'
 
 
 def _read(path: str) -> str:
@@ -22,6 +23,7 @@ def test_about_page_preserves_research_origin_and_current_affiliation() -> None:
     assert "A question I never stopped asking" not in page
     assert '<a href="https://triumf.ca/">TRIUMF, Canada\'s Particle Accelerator Centre</a>' in page
     assert f'<a href="{CONNECTED_CARE_URL}">Connected Care</a>' in page
+    assert f'<a href="{LINKEDIN_URL}">Mahsa Keikha, PhD</a>' in page
 
 
 def test_about_page_connects_foundations_to_human_measurement_research() -> None:
@@ -55,4 +57,5 @@ def test_homepage_exposes_about_researcher_path() -> None:
     assert "A question I never stopped asking" not in home
     assert '<a href="https://triumf.ca/">TRIUMF, Canada\'s Particle Accelerator Centre</a>' in home
     assert f'<a href="{CONNECTED_CARE_URL}">Connected Care</a>' in home
+    assert f'<a href="{LINKEDIN_URL}">Mahsa Keikha, PhD</a>' in home
     assert "mahsa@connectioncare.net" in home
